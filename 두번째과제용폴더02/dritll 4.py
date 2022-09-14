38eb2cad8bf653e62c0690c56ad22cf0758884c6
GwonYong Lee <dlrnjsdyd777@naver.com>
2022-09-14 17:16:43 +0900

    Drill #4

---
+++ 두번째과제용폴더02/dritll 4.py
@@ -0,0 +1,96 @@
+import turtle
+
+count=0
+
+while (count<4):
+    turtle.forward(100)
+    count+=1
+    
+turtle.left(90)
+count=0
+
+while (count<4):
+    turtle.forward(100)
+    count+=1
+    
+turtle.left(90)
+count=0
+
+while (count<4):
+    turtle.forward(100)
+    count+=1
+    
+turtle.left(90)
+count=0
+
+while (count<4):
+    turtle.forward(100)
+    count+=1
+
+    
+turtle.left(90)
+count=0
+
+
+turtle.forward(100)
+turtle.left(90)
+
+while (count<4):
+  turtle.forward(100)
+  count+=1
+
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+count=0
+
+while(count<4):
+  turtle.forward(100)
+  count+=1
+
+count = 0
+
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+while(count<4):
+  turtle.forward(100)
+  count+=1
+
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+count=0
+
+turtle.forward(100)
+turtle.right(90)
+
+while(count<4):
+  turtle.forward(100)
+  count+=1
+
+count=0
+
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+while(count<4):
+  turtle.forward(100)
+  count+=1
+
+count=0
+
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+while(count<4):
+  turtle.forward(100)
+  count+=1
+
+
+
+
